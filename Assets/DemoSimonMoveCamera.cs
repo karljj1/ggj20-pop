d5993e7d63b7576e145f15d6e72be7f921661e2c
Simonkjj <simon.jac@hotmail.com>
2020-02-01 23:28:59 +0100

    Kopi av kamilla level + anim
    Ingenting polert- første forsøk på å merge sammen level og karakteren til fredrik. GLHF ALL

---
+++ Assets/DemoSimonMoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoSimonMoveCamera : MonoBehaviour
+{
+    public float CameraSpeed = 5;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.position += new Vector3(-CameraSpeed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.position += new Vector3(CameraSpeed * Time.deltaTime, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+            transform.position += new Vector3(0, Time.deltaTime * CameraSpeed, 0);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.position += new Vector3(0, Time.deltaTime* -CameraSpeed, 0);
+
+    }
+}
