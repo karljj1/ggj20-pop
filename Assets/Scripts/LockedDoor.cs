22e15a7523f1e62f9eb4c423fc7b7ad37ab1550b
Fredrik Haugen <fred.haugen98@gmail.com>
2020-02-02 14:59:41 +0100

    Doors_final_final(3)

--- Assets/Scripts/LockedDoor.cs
+++ Assets/Scripts/LockedDoor.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LockedDoor : MonoBehaviour
 {
 	Animator anim;
+	public bool isOpen;
 
 	private void Awake()
 	{
@@ -21,6 +23,19 @@ public class LockedDoor : MonoBehaviour
 			anim.SetBool("doorOpen", true);
 
 			//Open door if time;
+		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+
+		if (collision.tag == "Player" && isOpen)
+		{
+			SceneManager.LoadScene("EndCredits");
+		}
+		else if (collision.tag == "Player" && !isOpen && collision.gameObject.GetComponent<SeedCharacterController>().keys > 0)
+		{
+			anim.SetBool("doorOpen", true);
 		}
 	}
 }
