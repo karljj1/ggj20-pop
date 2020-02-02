3a13a9e6bee70cef1ae635ba75dabac15f6d0e29
Fredrik Haugen <fred.haugen98@gmail.com>
2020-02-02 14:08:28 +0100

    Doors part 1

---
+++ Assets/Scripts/LockedDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+	Animator anim;
+
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+		anim.SetBool("doorOpen", false);
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<SeedCharacterController>().keys > 0)
+		{
+			collision.gameObject.GetComponent<SeedCharacterController>().keys--;
+
+			anim.SetBool("doorOpen", true);
+
+			//Open door if time;
+		}
+	}
+}
